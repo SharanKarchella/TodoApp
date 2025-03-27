47575b0e0109b26e4d1acab3ae68caa1eb139f70
SharanKarchella <sharankarchella@gmail.com>
2025-03-27 11:25:52 +0530

    Added db

---
+++ db.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb://localhost:27017/TodoApp_27_Mar_2025");
+
+
+const todoSchema = mongoose.Schema({
+
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todos = mongoose.model("todos", todoSchema);
+
+module.exports={
+    todos
+}
